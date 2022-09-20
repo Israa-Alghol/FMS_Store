e6cafa910d68faf853a64e9dbe90ac21c054b184
Israa Alghoul <ialghoul@fms-tech.com>
2022-09-20 12:45:26 +0300

    Start Api

---
+++ FMS_Store/Api/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMS_Store.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
