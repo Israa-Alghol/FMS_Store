0832ddd4bfeccc2e923e9aa0aa9f65028b4f77ee
Israa Alghoul <israa.fms@gmail.com>
2022-09-29 16:37:04 +0300

    Way To Remove

--- FMS_Store.Tests/DummyDataDBInitializer.cs
+++ FMS_Store.Tests/DummyDataDBInitializer.cs
@@ -1,3 +1,4 @@
+using FMS_Store.Models;
 using System;
 using Xunit;
 
@@ -5,9 +6,27 @@ namespace FMS_Store.Tests
 {
     public class DummyDataDBInitializer
     {
-        [Fact]
-        public void Test1()
+        public DummyDataDBInitializer()
         {
+        }
+
+        public void Seed(ProductDBContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+
+            context.Categories.AddRange(
+                new Category() { Type = "Games" },
+                new Category() { Type = "Sports' Wear" },
+                new Category() { Type = "Plants" }
+            );
+
+            context.Products.AddRange(
+            new Product() { Name = "Test Title 1", Price = 2000, Description = "Test Description 1", ImageUrl = "", CategoryId = 2 },
+            new Product() { Name = "Test Title 2", Price = 3000, Description = "Test Description 2", ImageUrl = "", CategoryId = 3 }
+            );
+            context.SaveChanges();
 
         }
     }
--- FMS_Store.Tests/Repository/ProductRepositoryTest.cs
+++ FMS_Store.Tests/Repository/ProductRepositoryTest.cs
@@ -1,10 +1,92 @@
-﻿using System;
+﻿using FluentAssertions;
+using FMS_Store.Api;
+using FMS_Store.Repositories;
+using FMS_Store.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace FMS_Store.Tests.Repository
 {
-    internal class ProductRepositoryTest
+    public class ProductRepositoryTest
     {
+        private ProductDbRepository repository;
+        private ProductDbRepository repository2;
+        public static DbContextOptions<ProductDBContext> dbContextOptions { get; }
+        public static string connectionString = "Server=ABCD;Database=Samples;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        static ProductRepositoryTest()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ProductDBContext>()
+               .UseSqlServer(connectionString)
+               .Options;
+
+
+
+        }
+        public ProductRepositoryTest()
+        {
+            var context = new ProductDBContext(dbContextOptions);
+            DummyDataDBInitializer db = new DummyDataDBInitializer();
+            db.Seed(context);
+
+            repository = new ProductDbRepository(context);
+
+        }
+
+
+        #region Get By Id
+
+        [Fact]
+        public void Task_GetPostById_Return_OkResult()
+        {
+            //Arrange
+            var controller = new ProductApiController(repository, (IRepo<Models.Category>)repository2);
+            var postId = 2;
+
+            //Act
+            var data = controller.Get(postId);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(data);
+
+        }
+        [Fact]
+        public void Task_GetPostById_Return_NotFoundResult()
+        {
+            //Arrange
+            var controller = new ProductApiController(repository, (IRepo<Models.Category>)repository2);
+            var postId = 3;
+
+            //Act
+            var data = controller.Get(postId);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(data);
+        }
+        [Fact]
+        public void Task_GetPostById_MatchResult()
+        {
+            //Arrange
+            var controller = new ProductApiController(repository, (IRepo<Models.Category>)repository2);
+            int? postId = 1;
+
+            //Act
+            var data = controller.Get((int)postId);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(data);
+
+            var okResult = data.Should().BeOfType<OkObjectResult>().Subject;
+            var product = okResult.Value.Should().BeAssignableTo<ProductCategoryViewModel>().Subject;
+
+            Assert.Equal("Test Title 1", product.Name);
+            Assert.Equal("Test Description 1", product.Description);
+        }
+
+        #endregion
     }
 }
